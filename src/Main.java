e615450a7e64a449da4ad1e05bb3833a85c20a66
faxcon08 <faxcon-08@mail.ru>
2022-11-03 16:08:16 +0300

    initial commit

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        String str="Hellow, word 2";
-        // System.out.println(str);
-
-    }
-}
